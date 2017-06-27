45873debc26263f349447c7f53dd8ec8b9dbb0a2
Jasleen <jasleen@xebia.com>
2017-06-27 12:45:57 +0530

    templating basics

--- secondSession/Components/src/app/app.component.ts
+++ secondSession/Components/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template : '<h1>{{myObject.name}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular 4 app';
+
+  myObject = {
+  name : 'Jasleen',
+  gender : 'female',
+  age : 24,
+  location : 'new delhi'
+  }
 }
